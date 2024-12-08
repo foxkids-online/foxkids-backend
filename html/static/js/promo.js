ed95c35e81eafaf9a55fa754a08aaa825d883536
Пользователь <nonofficial2015@gmail.com>
2024-12-08 21:34:16 +0300

    :hankey: Improve name showing at html code

--- html/static/js/promo.js
+++ html/static/js/promo.js
@@ -10,11 +10,11 @@ setInterval(function () {
             let current_series = data.current_series
             let next_series = data.next_series
             if (current_series) {
-                $('#promo-now-txt').text(current_series.name_rus);
+                $('#promo-now-txt').text(current_series.name_rus === "" ? current_series.name : current_series.name_rus);
                 $('#promo-now-img').css('background-image', `url('./static/sources/promos/${current_series.name}.png')`);
             }
             if (next_series) {
-                $('#promo-next-txt').text(next_series.name_rus);
+                $('#promo-next-txt').text(next_series.name_rus === "" ? next_series.name : next_series.name_rus);
                 $('#promo-next-img').css('background-image', `url('./static/sources/promos/${next_series.name}.png')`);
             }
         }
