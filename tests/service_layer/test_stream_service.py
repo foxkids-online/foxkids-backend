8c6807cb9f43ffb12634b002754d46c69a3e60eb
Пользователь <nonofficial2015@gmail.com>
2024-12-22 00:35:11 +0300

    :white_check_mark: Test for recreating series file increasing(disabled)

--- tests/service_layer/test_stream_service.py
+++ tests/service_layer/test_stream_service.py
@@ -1,6 +1,12 @@
 from pytest import mark
 
-from foxkids.adapters import FakeRepository, ScriptManager, Storage
+from foxkids.adapters import (
+    FakeRepository,
+    FileRepository,
+    ScriptManager,
+    Storage,
+)
+from foxkids.domain.features import get_series_list_by_day, increment_series
 from foxkids.service_layer import StreamService
 
 
@@ -11,3 +17,15 @@ def test_start_stream():
     script_manager = ScriptManager()
     service = StreamService(repository, storage, script_manager)
     service.start()
+
+
+@mark.skip(reason="попытка обновить дни стрима")
+def test_increase_series():
+    days = [1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6]
+    for day in days:
+        print(f"day={day}")
+        repository = FileRepository()
+        blocks = repository.get_blocks(day)  # пересчет серий
+        series_in_day = get_series_list_by_day(blocks)
+        updating_series = increment_series(series_in_day)
+        repository.update_series_list(updating_series)
